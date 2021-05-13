99212e1e8961d11a377a3a44b4b816218c4f3d8b
hudaokeji <github@hudaokeji.com>
2021-05-13 10:02:51 +0900

    update

--- dist/uncompressed/skylark-jquery-all.js
+++ dist/uncompressed/skylark-jquery-all.js
@@ -19052,9 +19052,56 @@ define('skylark-langx-urls/urls',[
 
 
 
-define('skylark-langx-urls/isCrossOrigin',[
+define('skylark-langx-urls/parseUrl',[
     './urls'
 ], function (urls) {
+    'use strict';
+    const parseUrl = function (url) {
+        const props = [
+            'protocol',
+            'hostname',
+            'port',
+            'pathname',
+            'search',
+            'hash',
+            'host'
+        ];
+        let a = document.createElement('a');
+        a.href = url;
+        const addToBody = a.host === '' && a.protocol !== 'file:';
+        let div;
+        if (addToBody) {
+            div = document.createElement('div');
+            div.innerHTML = `<a href="${ url }"></a>`;
+            a = div.firstChild;
+            div.setAttribute('style', 'display:none; position:absolute;');
+            document.body.appendChild(div);
+        }
+        const details = {};
+        for (let i = 0; i < props.length; i++) {
+            details[props[i]] = a[props[i]];
+        }
+        if (details.protocol === 'http:') {
+            details.host = details.host.replace(/:80$/, '');
+        }
+        if (details.protocol === 'https:') {
+            details.host = details.host.replace(/:443$/, '');
+        }
+        if (!details.protocol) {
+            details.protocol = window.location.protocol;
+        }
+        if (addToBody) {
+            document.body.removeChild(div);
+        }
+        return details;
+    };
+
+    return urls.parseUrl = parseUrl;
+});
+define('skylark-langx-urls/isCrossOrigin',[
+    './urls',
+    "./parseUrl"
+], function (urls,parseUrl) {
     'use strict';
 
     const isCrossOrigin = function (url, winLoc = window.location) {
